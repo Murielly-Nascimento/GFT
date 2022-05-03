7e36451d18f7610da41bc35a9b9949e18fabbf1c
Murielly-Nascimento <murielly.nascimento@ufu.br>
2022-05-03 14:18:38 -0300

    Primeiro commit

---
+++ Primeiro/src/main/Main.java
@@ -0,0 +1,12 @@
+package Primeiro.src.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int i = 0;
+        for(; i < 10; i++)
+            i+=2;
+        System.out.println(i);
+    }
+}
